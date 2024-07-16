ab14f4e3f586557212c4dcb06f3623a9c61200b1
gOps132 <epilred@icloud.com>
2024-07-16 23:21:45 +0800

    cleared error, it was a UB problem in the render variable. Added paddles and balls

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,17 @@
 #include <exception>
 #include <chrono>
 
+#define SCREEN_WIDTH 640	//window height
+#define SCREEN_HEIGHT 480	//window width
+
+// TODO: REFRACTOR USING THE C_NAMING CONVENTION
 
 class Window {
 public:
 	Window(
-		int p_width = 1080,
-		int p_height = 720,
-		std::string p_title = "title"
+		int p_width = SCREEN_WIDTH,
+		int p_height = SCREEN_HEIGHT,
+		std::string p_title = "Pong"
 	)
 	: m_width(p_width), m_height(p_height), m_title(p_title)
 	{
@@ -33,13 +37,13 @@ public:
 		SDL_DestroyWindow(m_window_instance);
 	};
 	
-	SDL_Window *get_instance() { return m_window_instance; }
-	void update_window_size() 
+	SDL_Window *getInstance() { return m_window_instance; }
+	void updateWindowSize() 
 	{
 		SDL_GetWindowSize(m_window_instance, &m_width, &m_height);
 	}
-	const int get_width() { return m_width; }
-	const int get_height() { return m_height; }
+	const int getWidth() { return m_width; }
+	const int getHeight() { return m_height; }
 
 private:
 	SDL_Window *m_window_instance;
@@ -48,19 +52,43 @@ private:
 	int m_height;
 };
 
+typedef struct Paddle { 
+	int x,y;
+	int width,height;
+} Paddle;
+
+typedef struct Ball {
+	int x,y;
+	int width,height;
+	int dx, dy;
+} Ball;
+
+// std::unique_ptr<int *> colorConvert(int p_color)
+// {
+//     // Extract RGB components
+//     uint8_t r = (p_color >> 16) & 0xFF;
+//     uint8_t g = (p_color >> 8) & 0xFF;
+//     uint8_t b = p_color & 0xFF;
+
+//     // Allocate memory for the array
+//     std::unique_ptr<int *> color = std::make_unique<int *>(new int[3]{r, g, b});
+//     return color;
+// }
+
+uint32_t colorConvert(SDL_Surface* surface, int p_color)
+{
+    // Extract RGB components
+    uint8_t r = (p_color >> 16) & 0xFF;
+    uint8_t g = (p_color >> 8) & 0xFF;
+    uint8_t b = p_color & 0xFF;
+
+	return SDL_MapSurfaceRGB(surface, r, g, b);
+}
+
+
 class Pong {
 public:
 	Pong() 
-	{
-		init();
-	}
-	~Pong()
-	{
-		SDL_DestroyRenderer(m_renderer);
-		SDL_Quit(); 
-		delete m_window;
-	};
-	void init()
 	{
 		m_window = new Window();
 
@@ -71,25 +99,69 @@ public:
 		}
 
 		// CREATE RENDERER
-		m_renderer = SDL_CreateRenderer(m_window->get_instance(), nullptr);
+		m_renderer = SDL_CreateRenderer(m_window->getInstance(), nullptr);
 		if (!m_renderer)
 		{
 			throw SDL_GetError();
 		}
+
+		m_screen = SDL_CreateSurface(m_window->getWidth(), m_window->getHeight(), SDL_PIXELFORMAT_RGBA8888);
+		// m_screen = SDL_CreateSurface(m_window->getWidth(), m_window->getHeight(), SDL_PIXELFORMAT_RGBA32);
+		if (!m_screen)
+		{
+			throw SDL_GetError();
+		}
+
+		initGame();
 	}
+	~Pong()
+	{
+		SDL_DestroySurface(m_screen);
+		SDL_DestroyRenderer(m_renderer);
+		delete m_window;
+		SDL_Quit(); 
+	};
+	// TODO:
+	void restart()
+	{
+		// delete stuff
+
+	}
+	void initGame()
+	{
+		// center ball
+		m_ball.x = m_screen->w/2; 
+		m_ball.y = m_screen->h/2;
+		m_ball.width = 10;
+		m_ball.height = 10;
+		m_ball.dy = 1;
+		m_ball.dx = 1;
+
+		m_paddle[0].x = 20;
+		m_paddle[0].y = (m_screen->h/2)-50;
+		m_paddle[0].width = 10;
+		m_paddle[0].height = 50;
+		
+		m_paddle[1].x = m_screen->w - 20;
+		m_paddle[1].y = (m_screen->h/2)-50;
+		m_paddle[1].width = 10;
+		m_paddle[1].height = 50;
+		
+		m_white_color = colorConvert(m_screen, 0xfffffff);
+	}
+
 	/**
 	 * poll events
 	 * check for  
 	 * Update ball position
 	 */
-	void game_loop()
+	void gameLoop()
 	{
 		auto start_time = std::chrono::steady_clock::now();
-		while (interrupt)
+		while (m_interrupt)
 		{
 			auto now = std::chrono::steady_clock::now();
     		auto time = std::chrono::duration_cast<std::chrono::seconds>(now-start_time);
-			time_elapsed = time.count();
 
 			// POLL EVENTS
 			while (SDL_PollEvent(&ev))
@@ -97,71 +169,103 @@ public:
 				{
 					case SDL_EventType::SDL_EVENT_QUIT:
 						throw "Application shutdown\n";
-						interrupt = false;
+						m_interrupt = false;
 						break;
 				}
 	
 			// frame rate
 			update(1.0/60.0);
 
-			std::cout << "time elapsed: " << time_elapsed << "\n";
+#ifdef DEBUG
+			m_time_elapsed = time.count();
+			std::cout << "time elapsed: " << m_time_elapsed << "\n";
+#endif
 		}
 	}
 
-	void update(double p_delta_time)
+	void drawBall() 
 	{
-		m_window->update_window_size();
+		SDL_Rect src;
 
-		SDL_Surface *m_surface = SDL_CreateSurface(m_window->get_width(), m_window->get_height(), SDL_PIXELFORMAT_RGBA8888);
-		if (!m_surface)
-		{
+		src.x = m_ball.x;
+		src.y = m_ball.y;
+		src.w = m_ball.width;
+		src.h = m_ball.height;
+
+// TODO: precompile dis
+		int r  = SDL_FillSurfaceRect(m_screen, &src, m_white_color);
+
+		if (r !=0){
 			throw SDL_GetError();
 		}
+	}
 
-		// Define the color 0x2ad873
-		uint32_t color = 0x2ad873;
+	void drawPaddle()
+	{
+		SDL_Rect src;
+		
+		for(int i = 0; i < 2; i++)
+		{
+			src.x = m_paddle[i].x; 
+			src.y = m_paddle[i].y; 
+			src.w = m_paddle[i].width; 
+			src.h = m_paddle[i].height; 
 
-		// Extract RGB components
-		uint8_t r = (color >> 16) & 0xFF;
-		uint8_t g = (color >> 8) & 0xFF;
-		uint8_t b = color & 0xFF;
+			int r  = SDL_FillSurfaceRect(m_screen, &src, m_white_color);
+			
+			if (r !=0){
+				throw SDL_GetError();
+			}
+		}
+	}
 	
+// TODO: window resizing event implementation later??
+	void update(double p_delta_time)
+	{
+		// m_window->updateWindowSize();
+
 		// map the color to the pixel format of the surface
-		uint32_t pixel_color = SDL_MapSurfaceRGB(m_surface, r, g, b);
+		// uint32_t color = colorConvert(m_screen, 0x2ad873);
 
-		// std::vector<uint32_t> pixels(m_window.width * m_window.height, 0);
-		// std::fill_n(pixels.data(), m_window.width * m_window.height, pixel_color);
+		SDL_Rect rect = {0,0,m_window->getWidth(),m_window->getHeight()}; // rectangle covering the entire surface
+		SDL_FillSurfaceRect(m_screen, &rect, 0x7F3A89FF);
+		
+		drawBall();
+		drawPaddle();
 
-		SDL_Rect rect = {0,0,m_window->get_width(),m_window->get_height()}; // rectangle covering the entire surface
-		SDL_FillSurfaceRect(m_surface, &rect, pixel_color);
-
-		SDL_Texture *m_texture = SDL_CreateTextureFromSurface(m_renderer, m_surface);
+		SDL_Texture *m_texture = SDL_CreateTextureFromSurface(m_renderer, m_screen);
 		if (!m_texture)
 		{
 			throw SDL_GetError();
 		}
-		SDL_DestroySurface(m_surface);
 
 		// RENDER
-		// SDL_UpdateTexture(m_texture, nullptr, pixels.data(), 4 * m_window.width);
 		SDL_RenderClear(m_renderer);
+		SDL_UpdateTexture(m_texture, nullptr, m_screen->pixels, m_screen->w * sizeof(uint32_t));
 		SDL_RenderTexture(m_renderer, m_texture, nullptr, nullptr);
-		SDL_RenderPresent(m_renderer);
-		 
+		SDL_RenderPresent(m_renderer);  
 	}
 private:
 	Window *m_window = nullptr;
 	SDL_Renderer *m_renderer = nullptr;
 	SDL_Event ev;
-	bool interrupt = true;
-	double time_elapsed;
+
+	SDL_Surface *m_screen;
+
+	Paddle m_paddle[2];
+	Ball m_ball;
+
+	bool m_interrupt = true;
+	double m_time_elapsed;
+
+	int m_white_color;
 };
 
 int main(int argc, char **argv)
 {
 		Pong *app = new Pong();
 		try {
-			app->game_loop();
+			app->gameLoop();
 		}
 		catch (const char* e) {
 			std::cout << e;
